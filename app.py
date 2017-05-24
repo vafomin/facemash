2b9bb59218ba24a3eefad0881010060b30670d34
Viktor <enotcode@gmail.com>
2017-05-24 22:32:02 +0300

    Commit🦄

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import random
 
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -12,12 +12,20 @@ db = client["facemash"]
 
 
 @app.route('/')
-def hello_world():
+def index():
     f = db.data.find_one({"id": random.randint(1, db.data.count())})
     s = db.data.find_one({"id": random.randint(1, db.data.count())})
     if f == s:
         s = db.data.find_one({"id": random.randint(1, db.data.count())})
-    return render_template('index.html', first=str(f), second=str(s))
+    return render_template('index.html', first=str(f["id"]), second=str(s["id"]))
+
+
+@app.route('/vote/<v>')
+def vote(v):
+    vote = db.data.find_one({"id": int(v)})["vote"] + 1
+    db.data.update({"id": int(v)}, {"$set":{"vote":vote}})
+    print(db.data.find_one({"id": int(v)}))
+    return redirect(url_for('index'), 302)
 
 
 if __name__ == '__main__':
