3178919b9132b2157c634218dbad2a662bf1a41c
Viktor <enotcode@gmail.com>
2017-06-02 23:11:05 +0300

    Update🦄

--- db.py
+++ db.py
@@ -1,5 +1,9 @@
 # Data Base Creator  💾
+import os
 from pymongo import MongoClient
+
+path, dirs, files = os.walk("app/static/images").next()
+count = len(files)
 
 base = 'mongodb://localhost:21017'
 
@@ -8,5 +12,5 @@ db = client["shortly"]
 
 db.data
 
-for i in range(1,9):
+for i in range(1,count):
     db.data.save( {'id':i, 'vote':0} )
